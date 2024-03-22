faf2994d922a6d75e8ecc10f9972c17ddf44116f
Sergey Atroshchenko <kapxapot@gmail.com>
2024-03-22 22:52:32 +0700

    Refactors

---
+++ Money.cs
@@ -0,0 +1,30 @@
+namespace MoneyExample;
+
+public class Money(Currency currency, decimal amount)
+{
+    public Currency Currency { get; } = currency;
+    public decimal Amount { get; } = Math.Round(amount, 2);
+
+    public Money ConvertTo(Currency newCurrency)
+    {
+        var newAmount = Amount.Convert(Currency, newCurrency);
+        return new(newCurrency, newAmount);
+    }
+
+    public static implicit operator decimal(Money money) => money.Amount;
+
+    public static implicit operator int(Money money) => (int)money.Amount;
+
+    public static Money operator +(Money money, int amount) => money + (decimal)amount;
+
+    public static Money operator +(Money money, decimal amount) =>
+        new(money.Currency, money.Amount + amount);
+
+    public static Money operator +(Money money1, Money money2)
+    {
+        var converted = money2.ConvertTo(money1.Currency);
+        return money1 + converted.Amount;
+    }
+
+    public override string ToString() => $"{Amount} {Currency}";
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using MoneyExample;
+
+Money m1 = new(Currency.RUR, 100);
+Money m2 = new(Currency.USD, 200);
+int x = 1000;
+
+// converts number to money
+Console.WriteLine($"{m1} + {x} = {m1 + x} (expected: 1100 RUR)");
+
+// converts money to number
+Console.WriteLine($"{x} + {m1} = {x + m1} (expected: 1100)");
+
+// converts RUB to USD
+Console.WriteLine($"{m2} + {m1} = {m2 + m1} (expected: 201.11 USD)");
+
+// everything is converted to the first currency
+// converts $200 (m2) into RUR, adds 100 RUR, after that adds another 1000
+Console.WriteLine($"{m1} + {m2} + {x} = {m1 + m2 + x} (expected: 19152 RUR)");
+
+// everything is treated as numbers
+Console.WriteLine($"{x} + {m1} + {m2} = {x + m1 + m2} (expected: 1300)");
+
+// converts USD to RUR, then adds RUR to 1000 and gives a number
+Console.WriteLine($"{x} + ({m1} + {m2}) = {x + (m1 + m2)} (expected: 19152)");
