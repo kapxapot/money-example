7cc98c34643d0dade7160593df86272d4428bf91
Sergey Atroshchenko <kapxapot@gmail.com>
2024-03-29 22:10:42 +0400

    More examples and tidying

--- Money.cs
+++ Money.cs
@@ -3,27 +3,27 @@ namespace MoneyExample;
 public class Money(Currency currency, decimal amount)
 {
     public Currency Currency { get; } = currency;
-    public decimal Amount { get; } = Math.Round(amount, 2);
-
-    public Money ConvertTo(Currency newCurrency)
-    {
-        var newAmount = Amount.Convert(Currency, newCurrency);
-        return new(newCurrency, newAmount);
-    }
+    public decimal Amount { get; } = Math.Round(amount, 2, MidpointRounding.AwayFromZero);
 
     public static implicit operator decimal(Money money) => money.Amount;
 
     public static implicit operator int(Money money) => (int)money.Amount;
+
+    public static Money operator +(Money money1, Money money2)
+    {
+        var converted = money2.ConvertTo(money1.Currency);
+        return money1 + converted.Amount;
+    }
 
     public static Money operator +(Money money, int amount) => money + (decimal)amount;
 
     public static Money operator +(Money money, decimal amount) =>
         new(money.Currency, money.Amount + amount);
 
-    public static Money operator +(Money money1, Money money2)
+    public Money ConvertTo(Currency newCurrency)
     {
-        var converted = money2.ConvertTo(money1.Currency);
-        return money1 + converted.Amount;
+        var newAmount = Amount.Convert(Currency, newCurrency);
+        return new(newCurrency, newAmount);
     }
 
     public override string ToString() => $"{Amount} {Currency}";
--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ Console.WriteLine($"{x} + {m1} = {x + m1} (expected: 1100)");
 Console.WriteLine($"{m2} + {m1} = {m2 + m1} (expected: 201.11 USD)");
 
 // everything is converted to the first currency
-// converts $200 (m2) into RUR, adds 100 RUR, after that adds another 1000
+// converts $200 (m2) to RUR, adds 100 RUR, after that adds another 1000
 Console.WriteLine($"{m1} + {m2} + {x} = {m1 + m2 + x} (expected: 19152 RUR)");
 
 // everything is treated as numbers
@@ -22,3 +22,25 @@ Console.WriteLine($"{x} + {m1} + {m2} = {x + m1 + m2} (expected: 1300)");
 
 // converts USD to RUR, then adds RUR to 1000 and gives a number
 Console.WriteLine($"{x} + ({m1} + {m2}) = {x + (m1 + m2)} (expected: 19152)");
+
+// converts RUB to USD, then adds USD to 1000 and gives a number
+// note that 201.11 USD is converted to int and becomes 201 (!)
+Console.WriteLine($"{x} + ({m2} + {m1}) = {x + (m2 + m1)} (expected: 1201)");
+
+// additional test 1
+
+// Money m = new(Currency.RUR, 100);
+// int y = 200;
+// var sumMoney = m + y; // gives `300 RUR`
+// var sumInt = y + m; // gives (int)300
+// Console.WriteLine($"money: {sumMoney}, int: {sumInt}");
+
+// additional test 2
+
+// int TotalSum(int partOne, int partTwo) => partOne + partTwo;
+
+// Money m = new(Currency.USD, 100);
+// var total1 = TotalSum(m, 1000); // (int)1100
+// var total2 = TotalSum(1000, m); // (int)1100
+
+// Console.WriteLine($"total 1: {total1}, total 2: {total2}");
